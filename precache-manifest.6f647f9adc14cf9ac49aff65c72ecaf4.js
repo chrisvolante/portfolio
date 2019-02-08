57becb0ec4545fdde283b6a016957651cc761f86
Christopher Volante <chrisvolante@pm.me>
2019-02-08 17:14:25 -0500

    Updates

---
+++ precache-manifest.6f647f9adc14cf9ac49aff65c72ecaf4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d09fe694a17aca392677c431002c2a1a",
+    "url": "/portfolio/static/media/quiz-image.d09fe694.png"
+  },
+  {
+    "revision": "86eff003124cca57affd6f412db7a610",
+    "url": "/portfolio/static/media/novacation-image.86eff003.png"
+  },
+  {
+    "revision": "5e00a86af26346ab6a1cf0be0c0549f2",
+    "url": "/portfolio/static/media/budget-image.5e00a86a.png"
+  },
+  {
+    "revision": "d5f6886c247a2ea138825611c075df43",
+    "url": "/portfolio/static/media/addressbook-image.d5f6886c.png"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "8f4967c061830c6d3141",
+    "url": "/portfolio/static/js/main.8f4967c0.chunk.js"
+  },
+  {
+    "revision": "bc419a9e0fcbd0a8d373",
+    "url": "/portfolio/static/js/1.bc419a9e.chunk.js"
+  },
+  {
+    "revision": "8f4967c061830c6d3141",
+    "url": "/portfolio/static/css/main.37e32bfa.chunk.css"
+  },
+  {
+    "revision": "a8c16385b7ed9755378ef163d1b3ffb5",
+    "url": "/portfolio/index.html"
+  }
+];
