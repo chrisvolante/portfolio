660edf572ca8192abd4b6214c4e9e6b53f5dcd59
Christopher Volante <chrisvolante@pm.me>
2019-02-11 11:48:34 -0500

    changed title. added netlify deply.

--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -15,8 +15,8 @@ const WorkPage = () => {
           <figcaption>
             <h2>Address <span>Book</span></h2>
             <p className="icon-links">
-              <a href="https://github.com/chrisvolante/addressbook-client"><span className="icon-heart">Repo</span></a>
-              <a href="https://cryptic-fortress-69642.herokuapp.com/"><span className="icon-eye">Live</span></a>
+              <a href="https://github.com/chrisvolante/addressbook-client" target="_blank"><span className="icon-heart">Repo</span></a>
+              <a href="https://cryptic-fortress-69642.herokuapp.com/" target="_blank"><span className="icon-eye">Live</span></a>
             </p>
             <p className="description">Keep all your contacts in one place and accessible on any device.</p>
           </figcaption>
@@ -26,8 +26,8 @@ const WorkPage = () => {
           <figcaption>
             <h2>Budget <span>App</span></h2>
             <p className="icon-links">
-              <a href="https://github.com/chrisvolante/budget-app"><span className="icon-heart">Repo</span></a>
-              <a href="https://afternoon-anchorage-88698.herokuapp.com/"><span className="icon-eye">Live</span></a>
+              <a href="https://github.com/chrisvolante/budget-app" target="_blank"><span className="icon-heart">Repo</span></a>
+              <a href="https://afternoon-anchorage-88698.herokuapp.com/" target="_blank"><span className="icon-eye">Live</span></a>
             </p>
             <p className="description">Budget App lets you keep track of daily spending!</p>
           </figcaption>
@@ -37,8 +37,8 @@ const WorkPage = () => {
           <figcaption>
             <h2>No <span>Vacation</span></h2>
             <p className="icon-links">
-              <a href="https://github.com/chrisvolante/no-vacation-app"><span className="icon-heart">Repo</span></a>
-              <a href="https://chrisvolante.github.io/no-vacation-app/"><span className="icon-eye">Live</span></a>
+              <a href="https://github.com/chrisvolante/no-vacation-app" target="_blank"><span className="icon-heart">Repo</span></a>
+              <a href="https://chrisvolante.github.io/no-vacation-app/" target="_blank"><span className="icon-eye">Live</span></a>
             </p>
             <p className="description">No Vacation takes your mind elsewhere when your body is stuck at work..</p>
           </figcaption>
@@ -48,8 +48,8 @@ const WorkPage = () => {
           <figcaption>
             <h2>Quiz <span>App</span></h2>
             <p className="icon-links">
-              <a href="https://github.com/chrisvolante/budget-app"><span className="icon-heart">Repo</span></a>
-              <a href="https://chrisvolante.github.io/quiz-app/"><span className="icon-eye">Live</span></a>
+              <a href="https://github.com/chrisvolante/budget-app" target="_blank"><span className="icon-heart">Repo</span></a>
+              <a href="https://chrisvolante.github.io/quiz-app/" target="_blank"><span className="icon-eye">Live</span></a>
             </p>
             <p className="description">Test your musical knowledge on some of the Hits from 2018!</p>
           </figcaption>
